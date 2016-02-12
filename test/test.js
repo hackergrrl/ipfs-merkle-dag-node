46235cf7046f2ac07aa7c99e87584e1a99268c10
Stephen Whitmore <stephen.whitmore@gmail.com>
2016-02-12 10:03:08 -0800

    tests

---
+++ test/test.js
@@ -0,0 +1,73 @@
+var test = require('tape')
+var dag = require('../index')
+
+test('create a node', function (t) {
+  var dagN = new dag.Node(new Buffer('some data'))
+  t.assert(Buffer.isBuffer(dagN.data), 'is a buffer')
+  t.assert(dagN.data.length, 9)
+  t.equal(dagN.size, 11)
+  t.equal(dagN.size, dagN.encoded.length)
+  t.equal(dagN.multihash, 'Qmd7xRhW5f29QuBFtqu3oSD27iVy35NRB91XFjmKFhtgMr')
+  t.equal(dagN.links.length, 0)
+  t.end()
+})
+
+test('create an empty node', function (t) {
+  var dagN = new dag.Node(new Buffer(0))
+  t.assert(Buffer.isBuffer(dagN.data), 'is a buffer')
+  t.equal(dagN.data.length, 0)
+  t.equal(dagN.size, 0)
+  t.end()
+})
+
+test('re-encode equivalency', function (t) {
+  var dagN = new dag.Node('you\'re all right, kid.')
+
+  var reencodedNode = dag.fromProtobuf(dagN.encoded)
+  t.deepEqual(dagN.data, reencodedNode.data)
+  t.deepEqual(dagN.encoded, reencodedNode.encoded)
+  t.deepEqual(dagN.hash, reencodedNode.hash)
+  t.deepEqual(dagN.size, reencodedNode.size)
+  t.deepEqual(dagN.multihash, reencodedNode.multihash)
+  t.end()
+})
+
+test('create nodes with links', function (t) {
+  var node1 = new dag.Node('hello')
+  t.equal(node1.multihash, 'QmTnaGEpw4totXN7rhv2jPMXKfL8s65PhhCKL5pwtJfRxn')
+  t.deepEqual(node1.data, new Buffer('hello'))
+  t.equal(node1.size, 7)
+  t.deepEqual(node1.links, [])
+
+  var node2 = new dag.Node('world', [node1.asLink('prev')])
+  t.equal(node2.multihash, 'QmRCETTRRkDQsZqxYgPv8QbCdcxoL8SccXo6YTa8t9UMM3')
+  t.deepEqual(node2.data, new Buffer('world'))
+  t.equal(node2.size, 60)
+  t.equal(node2.links.length, 1)
+  var link = {
+    name: 'prev',
+    size: 7,
+    hash: new Buffer('122050ee8231ac5be6b674d35e806db2900dd4048450d29a8598559a9c0d088cc7e3', 'hex')
+  }
+  t.deepEqual(node2.links[0], link)
+
+  var node3 = new dag.Node('!!!', { 'prev': node1, 'otherPrev': node2 })
+  t.equal(node3.multihash, 'Qmd6KYy2Rb2wuTHwUFTKg8QqRd1TqSEeD6GULjJuSCtDCA')
+  t.deepEqual(node3.data, new Buffer('!!!'))
+  t.equal(node3.size, 169)
+  t.equal(node3.links.length, 2)
+  var link1 = {
+    name: 'prev',
+    size: 7,
+    hash: new Buffer('122050ee8231ac5be6b674d35e806db2900dd4048450d29a8598559a9c0d088cc7e3', 'hex')
+  }
+  var link2 = {
+    name: 'otherPrev',
+    size: 60,
+    hash: new Buffer('12202a6afde7895640b9098f8251ffb059d182b0888723ed182b112cfa40ba290f32','hex')
+  }
+  t.deepEqual(node3.links[0], link1)
+  t.deepEqual(node3.links[1], link2)
+
+  t.end()
+})
